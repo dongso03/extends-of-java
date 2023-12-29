b4d24b38b0b62914f4375e1bce424075cf40a420
dongsukim <dongso03@naver.com>
2023-12-29 10:34:05 +0900

    자식클래스에서 부모클래스 메소드 오버라이드 가능

--- 12.12.29-1extends/src/Main.java
+++ 12.12.29-1extends/src/Main.java
@@ -1,25 +1,28 @@
 class MySuper{
-	public void printHllo() {
+	public void printHello() {
 		System.out.println("hello");
 	}
 }
 
 class MySub extends MySuper	{
-	public void printHollo() {
-		super.printHllo();
+	// Method Override
+	// 부모의 클래스에서 저의된 메소드 (Head가 같은메소드)의 새구현(Body를 작성)
+	public void printHello() {
+		super.printHello();
 		System.out.println("hi");
 	}
 }
 
-
+//ㅇㅇ
 
 public class Main {
 	public static void main(String[] args) {
 		MySuper sup = new MySuper();
-		sup.printHllo();
+		sup.printHello();
 		System.out.println();
+		
 		MySub sub = new MySub();
-		sub.printHllo();
+		sub.printHello();
 		
 	}
 }
